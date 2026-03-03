2f0497bb0c033fe3e9ba26c9b6c539473c43f6ef
sameul-islam <sameulislam479@gmail.com>
2026-03-03 14:46:26 +0600

    create collection section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [visible, setVisible] = useState(false)
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
-        <img src={assets.logo} className='w-36' alt="" />
+      <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
           <NavLink to='/' className="flex flex-col items-center gap-1">
@@ -45,8 +48,25 @@ const Navbar = () => {
             </div>
             <Link to='/cart' className='relative'>
                <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+               <p className='absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
             </Link>
+            <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
 
+        </div>
+
+        {/* Sidebar menu for small screens */}
+
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : "w-0"}`}>
+           <div className='flex flex-col text-gray-600'>
+              <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3'>
+                <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="" />
+                <p>Back</p>
+              </div>
+              <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-t border-gray-200" to="/">HOME</NavLink>
+              <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-200" to="/collection">COLLECTION</NavLink>
+              <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-200" to="/about">ABOUT</NavLink>
+              <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-200" to="/contact">CONTACT</NavLink>
+           </div>
         </div>
       
     </div>
