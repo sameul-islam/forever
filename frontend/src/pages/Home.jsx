9f43e50ae2743f74f6c31e3d6517cdecca742566
sameul-islam <sameulislam479@gmail.com>
2026-03-04 22:04:45 +0600

    create bestseller and footer section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import Bestseller from '../components/Bestseller'
+import OurPolicy from '../components/OurPolicy'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
+      <Bestseller/>
+      <OurPolicy/>
+      <NewsLetterBox/>
     </div>
   )
 }
