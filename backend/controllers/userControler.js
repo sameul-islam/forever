b779500672f673633ad933660ed4f75b742b3e31
sameul-islam <sameulislam479@gmail.com>
2026-04-14 13:29:03 +0600

    complete mongodb connecting

---
+++ backend/controllers/userControler.js
@@ -0,0 +1,21 @@
+
+
+// Route for user login
+const loginUser = async (req,res) => {
+
+}
+
+
+// Route for user register
+const registerUser = async (req, res) => {
+
+    
+
+}
+
+// Route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudnary.js"
+import userRouter from "./routes/UserRoute.js"
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
    res.send("API Working")
