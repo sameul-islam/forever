715fff4ec86bfea5391b19400bef33b0d05bc93a
sameul-islam <sameulislam479@gmail.com>
2026-04-16 15:46:15 +0600

    start backend part

---
+++ backend/controllers/productController.js
@@ -0,0 +1,47 @@
+
+
+// function for add product
+const addProduct = async (req, res) => {
+    try {
+
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body
+
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined )
+
+        console.log(name, description, price, category, subCategory, sizes, bestseller);
+        console.log(image1,image2, image3, image4)
+
+        res.json({})
+        
+
+    } catch (error) {
+        console.log(error)
+         res.json({success: false, message: error.message})
+    }
+}
+
+
+// function for list product
+const listProduct = async (req, res) => {
+    
+}
+
+
+// function for removing product
+const removeProduct = async (req, res) => {
+    
+}
+
+
+// function for single product info
+const singleProduct = async (req, res) => {
+    
+}
+
+
+export {listProduct, addProduct, removeProduct, singleProduct}
