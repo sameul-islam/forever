343574dc03dcc5b73bec2905b8b46a302decf46f
sameul-islam <sameulislam479@gmail.com>
2026-04-20 13:33:44 +0600

    create product api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
-
+import { v2 as cloudinary } from 'cloudinary'
+import productModel from '../models/productModel.js'
 
 // function for add product
 const addProduct = async (req, res) => {
@@ -13,10 +14,30 @@ const addProduct = async (req, res) => {
 
         const images = [image1, image2, image3, image4].filter((item) => item !== undefined )
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(image1,image2, image3, image4)
+        const imagesUrl = await Promise.all(
+            images.map(async (item) => {
+               let result = await cloudinary.uploader.upload(item.path,{resource_type: 'image'});
+               return result.secure_url
+            })
+        )
+          
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
+        console.log(productData)
 
-        res.json({})
+        const product = new productModel(productData)
+        await product.save()
+
+        res.json({success: true, message: "Product Added"})
         
 
     } catch (error) {
@@ -28,19 +49,46 @@ const addProduct = async (req, res) => {
 
 // function for list product
 const listProduct = async (req, res) => {
+
+    try {
+       
+        const products = await productModel.find({});
+        res.json({success: true, products})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
     
 }
 
 
 // function for removing product
 const removeProduct = async (req, res) => {
-    
+    try {
+      
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: " Product Removed " })
+
+    }catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 
 // function for single product info
 const singleProduct = async (req, res) => {
-    
+    try {
+     
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message })
+    }
 }
 
 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -6,8 +6,8 @@ const productSchema = new mongoose.Schema({
     price: { type: Number, required: true },
     image: { type: Array, required: true},
     price: { type: Number, required: true},
-    caterory: { type: String, required: true},
-    subCategory: { type: Number, required: true},
+    category: { type: String, required: true},
+    subCategory: { type: String, required: true},
     sizes: { type: Array, required: true},
     bestseller: { type:Boolean},
     date: { type: Number, required: true }
