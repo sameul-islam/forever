b4816791ca2434c326ea8bd62ffb1570b3b8ac2c
sameul-islam <sameulislam479@gmail.com>
2026-05-16 20:56:57 +0600

    improve backend and frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -41,6 +41,17 @@ const ShopcontextProvider = (props) => {
         }
         setCartItems(cartData);
 
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers:{token} })
+                
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
+
     }
 
     const getCartCount = () => {
@@ -66,6 +77,18 @@ const ShopcontextProvider = (props) => {
         cartData[itemId][size] = quantity;
 
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/update', {itemId, size, quantity}, {headers:{token}} )
+                
+            } catch (error) {
+                console.log(error)
+                toast.error(error.message)
+            }
+        }
+
     }
 
     const getCartAmount = () => {
@@ -104,13 +127,28 @@ const ShopcontextProvider = (props) => {
         }
     }
 
+    const getUserCart = async ( token ) => {
+        try {
+            
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers: {token}})
+            if(response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
          getProductsData()
     }, []);
 
     useEffect(() =>{
         if (!token && localStorage.getItem('token')) {
-                setToken(localStorage.getItem('token'))
+            setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
         }
     },[]);
 
